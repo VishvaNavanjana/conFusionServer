eda2d8a8f6be4528e0857ad3d70030cbbf63d60a
VishvaNavanjana <vishvanavanjana@gmail.com>
2022-01-18 23:06:06 +0530

    deprecation warning resolved

--- app.js
+++ app.js
@@ -11,11 +11,22 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
+mongoose.Promise = Promise;
 
 const Dishes = require('./models/dishes');
 
+const options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url,  options);
 
 connect.then((db) =>{
   console.log('Connnected correctly to server');
