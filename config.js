c4e62c84fbea5135d2fd2a3050be8099ff57ae69
VishvaNavanjana <vishvanavanjana@gmail.com>
2022-02-08 23:06:07 +0530

    facebook OAuth login added using passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '928318801199379',
+        clientSecret:'4adf68ad8a82757cad6af130293fe6d2'
+    }
 }
